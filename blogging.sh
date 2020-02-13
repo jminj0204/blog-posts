d3064696504b56b8772a45e327b3376b603c3b07
jiminjeong22 <jiminjeong22@users.noreply.github.com>
2020-02-13 23:50:02 +0900

    2020-02-13_23-50-02

--- blogging.sh
+++ blogging.sh
@@ -4,12 +4,13 @@ git add --all
 git commit -a -m $(date +%F_%H-%M-%S) #	2020-02-13_21-25-40
 git push
 
-# cleanup "_site"
-rm -rf _site
-mkdir _site
-
 # clone remote repo to "_site"
-git clone https://github.com/jiminjeong22/jiminjeong22.github.io.git _site
+#git clone https://github.com/jiminjeong22/jiminjeong22.github.io.git _site
+
+# cleanup "_site"
+cd _site
+rm -v !("./git")
+cd ../
 
 # build with Jekyll into "_site"
 bundle exec jekyll build
